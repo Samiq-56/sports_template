5b615bff49aff0fdff2db27c3c1f967ebfb39cd4
Samiq-56 <bsdsf24m056@pucit.edu.pk>
2024-12-30 12:25:02 +0500

    Add files via upload

---
+++ JAVA.js
@@ -0,0 +1,72 @@
+function first(){
+    alert("Welcome To my Website");
+ }
+
+function second(){
+    document.getElementById("card-img").src = "messi2.jpg";
+ }
+ function third(){
+    document.getElementById("card-img1").src = "ronaldo2.jpg";
+ }
+ function forth(){
+    document.getElementById("card-img2").src = "naymer.jpg";
+ }
+function secondLeave(){
+    document.getElementById("card-img").src = "messi.jpg";
+ }
+ function thirdLeave(){
+    document.getElementById("card-img1").src = "ronaldo.jpg";
+ }
+ function forthLeave(){
+    document.getElementById("card-img2").src = "naymer4.jpg";
+ }
+
+ function contact(){
+   alert("Thankyou For your Response");
+}
+function color_change1(){
+   document.getElementById("change_color1").style.color='navy';
+
+}
+
+function color_BackChange1(){
+   document.getElementById("change_color1").style.color='black';
+
+}
+
+
+
+
+
+function color_change2() {
+   document.getElementById("color_change2").style.color = 'navy';
+ }
+
+ function color_BackChange2() {
+   document.getElementById("color_change2").style.color = 'black';
+ }
+
+
+
+
+ function color_change3() {
+   document.getElementById("color_change3").style.color = 'navy';
+ }
+
+ function color_BackChange3() {
+   document.getElementById("color_change3").style.color = 'black';
+ }
+
+
+
+
+
+ 
+ function color_change4() {
+   document.getElementById("color_change3").style.color = 'navy';
+ }
+
+ function color_BackChange4() {
+   document.getElementById("color_change3").style.color = 'black';
+ }
+
